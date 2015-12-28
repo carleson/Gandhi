09b451a47c3f59c14899bd553a4d1c2b290d72fa
Johan Carleson <johan.carleson@gmail.com>
2015-12-28 22:59:40 +0100

    Added some config code

--- src/app.js
+++ src/app.js
@@ -8,6 +8,42 @@
  *
  */
 
+/*
+Pebble.addEventListener('ready', function() {
+  console.log('PebbleKit JS ready!');
+});
+
+Pebble.addEventListener('showConfiguration', function() {
+  var url = 'http://carleson.github.io/Pebble/Gandhi/index.html';
+  console.log('Showing configuration page: ' + url);
+
+  Pebble.openURL(url);
+});
+
+Pebble.addEventListener('webviewclosed', function(e) {
+  var configData = JSON.parse(decodeURIComponent(e.response));
+  console.log('Configuration page returned: ' + JSON.stringify(configData));
+
+  var backgroundColor = configData['background_color'];
+
+  var dict = {};
+  if(configData['high_contrast'] === true) {
+    dict['KEY_HIGH_CONTRAST'] = configData['high_contrast'] ? 1 : 0;  // Send a boolean as an integer
+  } else {
+    dict['KEY_COLOR_RED'] = parseInt(backgroundColor.substring(2, 4), 16);
+    dict['KEY_COLOR_GREEN'] = parseInt(backgroundColor.substring(4, 6), 16);
+    dict['KEY_COLOR_BLUE'] = parseInt(backgroundColor.substring(6), 16);
+  }
+
+  // Send to watchapp
+  Pebble.sendAppMessage(dict, function() {
+    console.log('Send successful: ' + JSON.stringify(dict));
+  }, function() {
+    console.log('Send failed!');
+  });
+});
+
+*/
 var UI = require('ui');
 var Vector2 = require('vector2');
 var Accel = require('ui/accel');
@@ -44,6 +80,7 @@ var image = new UI.Image({
   size: new Vector2(144, 168),
   image: 'images/Gandhi.png'
 });
+
 wind.add(image);
 wind.show();
 
@@ -86,6 +123,7 @@ function GetRandomPage(){
   Page = number;  
   return number;
 }
+
 
 // Prepare the accelerometer
 Accel.init();
