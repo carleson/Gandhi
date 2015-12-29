d1ea0615559423ded3b73f2d70727b8db71f89cd
Johan Carleson <johan.carleson@gmail.com>
2015-12-29 23:54:43 +0100

    refactoring

--- src/app.js
+++ src/app.js
@@ -19,9 +19,35 @@ var Accel = require('ui/accel');
 var Vibe = require('ui/vibe');
 var Settings = require('settings');
 
-/* Default values */
+// GLOBAL VARIABLES
 var showSplashscreen = true;
 var splashscreenTimer = 0;
+var Page = 0;
+var Quotes = [
+  "“Be the change you want to see in the world”",
+  "“First they ignore you, then they laught at you, then they fight you, then you win”",
+  "“Those who know how to think need no teachers”",
+  "“There is more to life than increasing itś speed”",
+  "”Where there is love there is life”",
+  "”An eye for an eye will make the whole world blind”",
+  "”Live as if you were to die tomorrow. Learn as if you were to live forever”",
+  "”the best way to find yourself is to lose yourself in the service of others”",
+  "”God has no religion”",
+  "”Happiness is when what you think, what you say and what you do are in harmony”",
+  "”Keep your thoughts positive - because your thoughts become your words”",
+  "”Keep your words positive - because your words become your behavior”",
+  "”Keep your behavior positive - because you behavior becomes your habits,”",
+  "”Keep your habits positive - because your habits become your values”",
+  "”Keep your values positive - because your values become your destiny”",
+  "”Poverty is the worst form of violence”",
+  "”Freedom is not worth having if it does not include the freedom to make mistakes”",
+  "”Truth never damages a cause that is just”",
+  "”It is health that is real wealth and not pieces of gold and silver”",
+  "”My life is my message”",
+  "”A nation's culture resides in the hearts and in the soul of its people”",
+  "”If I had no sense of humor, I would long ago have committed suicide”",
+  "”In a gentle way, you can shake the world”"
+];
 
 // CONFIGURATION
 // WINDOWS
@@ -65,32 +91,6 @@ Pebble.addEventListener('webviewclosed', function(e) {
   });
 });
 
-var Quotes = [
-  "“Be the change you want to see in the world”",
-  "“First they ignore you, then they laught at you, then they fight you, then you win”",
-  "“Those who know how to think need no teachers”",
-  "“There is more to life than increasing itś speed”",
-  "”Where there is love there is life”",
-  "”An eye for an eye will make the whole world blind”",
-  "”Live as if you were to die tomorrow. Learn as if you were to live forever”",
-  "”the best way to find yourself is to lose yourself in the service of others”",
-  "”God has no religion”",
-  "”Happiness is when what you think, what you say and what you do are in harmony”",
-  "”Keep your thoughts positive - because your thoughts become your words”",
-  "”Keep your words positive - because your words become your behavior”",
-  "”Keep your behavior positive - because you behavior becomes your habits,”",
-  "”Keep your habits positive - because your habits become your values”",
-  "”Keep your values positive - because your values become your destiny”",
-  "”Poverty is the worst form of violence”",
-  "”Freedom is not worth having if it does not include the freedom to make mistakes”",
-  "”Truth never damages a cause that is just”",
-  "”It is health that is real wealth and not pieces of gold and silver”",
-  "”My life is my message”",
-  "”A nation's culture resides in the hearts and in the soul of its people”",
-  "”If I had no sense of humor, I would long ago have committed suicide”",
-  "”In a gentle way, you can shake the world”"
-];
-var Page = 0;
 var wind = new UI.Window({ fullscreen: true });
 var image = new UI.Image({
   position: new Vector2(0, 0),
